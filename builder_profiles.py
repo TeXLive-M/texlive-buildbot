6038ab2a1a2bf8f63d95538fb012f6ba7c39c9b0
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2018-03-08 10:59:04 +0100

    update builder_profiles for C++11

--- builder_profiles.py
+++ builder_profiles.py
@@ -38,36 +38,40 @@ class BuildWorker(object):
         self.cxx11    = profile.cxx11
 
 env_darwin10 = {}
-env_darwin10_cxx11 = {}
 for arch in ['ppc', 'i386', 'x86_64']:
-    env_darwin10[arch]       = {}
-    env_darwin10_cxx11[arch] = {}
+    env_darwin10[arch] = {}
+    cc = {}
+    cxx = {}
 
-    if arch == 'x86_64':
-        target = '10.6'
-    else:
-        target = '10.5'
-    libdir  = '-L/Developer/SDKs/MacOSX{}.sdk/usr/lib'.format(target)
-    sysroot = '-isysroot /Developer/SDKs/MacOSX{}.sdk -mmacosx-version-min={}'.format(target, target)
-    cc      = '/usr/bin/gcc-4.2 -arch {}'.format(arch)
-    cxx     = '/usr/bin/g++-4.2 -arch {}'.format(arch)
-    cc11    = 'clang-mp-3.9 -arch {}'.format(arch)
-    cxx11   = 'clang++-mp-3.9 -stdlib=libc++ -arch {}'.format(arch)
-    ldflags = libdir + ' ' + sysroot
+    for stdlib in ['', 'libc++', 'libstdc++']:
+        env_darwin10[arch][stdlib] = {}
 
-    env_darwin10[arch]['CC']     = cc
-    env_darwin10[arch]['OBJC']   = cc
-    env_darwin10[arch]['CXX']    = cxx
-    env_darwin10[arch]['OBJCXX'] = cxx
+    cc[''] = '/usr/bin/gcc-4.2'
+    cxx[''] = '/usr/bin/g++-4.2'
+    cc['libc++'] = '/opt/local/bin/clang-mp-5.0'
+    cxx['libc++'] = '/opt/local/bin/clang++-mp-5.0 -stdlib=libc++'
+    cc['libstdc++'] = '/opt/local/bin/clang-mp-5.0'
+    cxx['libstdc++'] = '/opt/local/bin/clang++-mp-5.0 -stdlib=macports-libstdc++ -D_GLIBCXX_USE_CXX11_ABI=0'
 
-    for flags in ['CFLAGS', 'OBJCFLAGS', 'CXXFLAGS', 'OBJCXXFLAGS']:
-        env_darwin10[arch][flags]       = sysroot
-        env_darwin10_cxx11[arch][flags] = sysroot
-    env_darwin10[arch]['LDFLAGS']       = ldflags
-    env_darwin10_cxx11[arch]['LDFLAGS'] = ldflags
+    for stdlib in ['', 'libc++', 'libstdc++']:
+        if arch == 'x86_64':
+            target = '10.6'
+        else:
+            target = '10.5'
+        libdir  = '-L/Developer/SDKs/MacOSX{}.sdk/usr/lib'.format(target)
+        sysroot = '-isysroot /Developer/SDKs/MacOSX{}.sdk -mmacosx-version-min={}'.format(target, target)
+        ldflags = libdir + ' ' + sysroot
 
-    env_darwin10[arch]['STRIP']       = 'strip -u -r'
-    env_darwin10_cxx11[arch]['STRIP'] = 'strip -u -r'
+        env_darwin10[arch][stdlib]['CC']     = '{} -arch {}'.format(cc[stdlib], arch)
+        env_darwin10[arch][stdlib]['OBJC']   = '{} -arch {}'.format(cc[stdlib], arch)
+        env_darwin10[arch][stdlib]['CXX']    = '{} -arch {}'.format(cxx[stdlib], arch)
+        env_darwin10[arch][stdlib]['OBJCXX'] = '{} -arch {}'.format(cxx[stdlib], arch)
+
+        for flags in ['CFLAGS', 'OBJCFLAGS', 'CXXFLAGS', 'OBJCXXFLAGS']:
+            env_darwin10[arch][stdlib][flags] = sysroot
+        env_darwin10[arch][stdlib]['LDFLAGS'] = ldflags
+
+        env_darwin10[arch][stdlib]['STRIP'] = 'strip -u -r'
 
 env_solaris10 = {
     'CC'      : '/opt/csw/bin/gcc-5.5',
@@ -107,15 +111,20 @@ builder_profiles = {
     'solaris10-sparc'  : BuilderProfile(platform = 'solaris', env = env_solaris10,          cmd_make = 'gmake', cmd_tar = 'gtar',   cxx11 = True),
     'solaris10-i386'   : BuilderProfile(platform = 'solaris', env = env_solaris10,          cmd_make = 'gmake', cmd_tar = 'gtar',   cxx11 = True),
     'solaris10-x86_64' : BuilderProfile(platform = 'solaris', env = env_solaris10_64,       cmd_make = 'gmake', cmd_tar = 'gtar',   cxx11 = True),
-    'darwin10-i386'    : BuilderProfile(platform = 'darwin',  env = env_darwin10['i386'],   cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = False),
-    'darwin10-x86_64'  : BuilderProfile(platform = 'darwin',  env = env_darwin10['x86_64'], cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = False),
-    'darwin10-powerpc' : BuilderProfile(platform = 'darwin',  env = env_darwin10['ppc'],    cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = False),
+    'freebsd'          : BuilderProfile(platform = 'freebsd', env = env_freebsd,            cmd_make = 'gmake', cmd_tar = 'gtar',   cxx11 = True),
     'openbsd'          : BuilderProfile(platform = 'openbsd', env = env_openbsd,            cmd_make = 'gmake', cmd_tar = 'gtar',   cxx11 = False),
     'openbsd_cxx11'    : BuilderProfile(platform = 'openbsd', env = env_openbsd_cxx11,      cmd_make = 'gmake', cmd_tar = 'gtar',   cxx11 = True),
-    'linux'            : BuilderProfile(platform = 'linux',   env = env_linux,              cmd_make = 'make',  cmd_tar = 'tar',    cxx11 = False),
-    'linux_cxx11'      : BuilderProfile(platform = 'linux',   env = env_linux,              cmd_make = 'make',  cmd_tar = 'tar',    cxx11 = True),
+    'linux'            : BuilderProfile(platform = 'linux',   env = env_linux,              cmd_make = 'make',  cmd_tar = 'tar',    cxx11 = True),
     'linux-mingw32'    : BuilderProfile(platform = 'mingw',   env = env_mingw['32'],        cmd_make = 'make',  cmd_tar = 'tar',    cxx11 = True),
     'linux-mingw64'    : BuilderProfile(platform = 'mingw',   env = env_mingw['64'],        cmd_make = 'make',  cmd_tar = 'tar',    cxx11 = True),
+
+    'darwin10-i386'             : BuilderProfile(platform = 'darwin',  env = env_darwin10['i386'][''],            cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = False),
+    'darwin10-x86_64'           : BuilderProfile(platform = 'darwin',  env = env_darwin10['x86_64'][''],          cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = False),
+    'darwin10-powerpc'          : BuilderProfile(platform = 'darwin',  env = env_darwin10['ppc'][''],             cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = False),
+    'darwin10_libc++-i386'      : BuilderProfile(platform = 'darwin',  env = env_darwin10['i386']['libc++'],      cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = True),
+    'darwin10_libc++-x86_64'    : BuilderProfile(platform = 'darwin',  env = env_darwin10['x86_64']['libc++'],    cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = True),
+    'darwin10_libstdc++-i386'   : BuilderProfile(platform = 'darwin',  env = env_darwin10['i386']['libstdc++'],   cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = True),
+    'darwin10_libstdc++-x86_64' : BuilderProfile(platform = 'darwin',  env = env_darwin10['x86_64']['libstdc++'], cmd_make = 'make',  cmd_tar = 'gnutar', cxx11 = True),
 }
 
 # slave:   name of the slave
@@ -125,20 +134,23 @@ builder_list = [
     BuildWorker(slave = 'solaris10-i386',              code = 'csw', profile = builder_profiles['solaris10-i386'],   name = 'solaris-i386.csw',         arch = 'i386',    tlname = 'i386-solaris',        upload = True),
     BuildWorker(slave = 'solaris10-i386',              code = 'csw', profile = builder_profiles['solaris10-x86_64'], name = 'solaris-x86_64.csw',       arch = 'x86_64',  tlname = 'x86_64-solaris',      upload = True),
     BuildWorker(slave = 'solaris10-sparc',             code = 'csw', profile = builder_profiles['solaris10-sparc'],  name = 'solaris-sparc.csw',        arch = 'sparc',   tlname = 'sparc-solaris',       upload = True),
-    BuildWorker(slave = 'darwin10-x86_64',             code = 'prg', profile = builder_profiles['darwin10-powerpc'], name = 'darwin-powerpc.prg',       arch = 'powerpc', tlname = 'powerpc-darwin',      upload = True),
-    BuildWorker(slave = 'darwin10-x86_64',             code = 'prg', profile = builder_profiles['darwin10-i386'],    name = 'darwin-i386.prg',          arch = 'i386',    tlname = 'i386-darwin',         upload = True),
-    BuildWorker(slave = 'darwin10-x86_64',             code = 'prg', profile = builder_profiles['darwin10-x86_64'],  name = 'darwin-x86_64.prg',        arch = 'x86_64',  tlname = 'x86_64-darwinlegacy', upload = True),
-    BuildWorker(slave = 'pragma-openbsd6.0-i386',      code = 'prg', profile = builder_profiles['openbsd'],          name = 'openbsd-i386-6.0.prg',     arch = 'i386',    tlname = 'i386-openbsd6.0',     upload = True),
-    BuildWorker(slave = 'pragma-openbsd6.1-i386',      code = 'prg', profile = builder_profiles['openbsd'],          name = 'openbsd-i386-6.1.prg',     arch = 'i386',    tlname = 'i386-openbsd6.1',     upload = True),
-    BuildWorker(slave = 'pragma-openbsd6.0-amd64',     code = 'prg', profile = builder_profiles['openbsd'],          name = 'openbsd-amd64-6.0.prg',    arch = 'amd64',   tlname = 'amd64-openbsd6.0',    upload = True),
-    BuildWorker(slave = 'pragma-openbsd6.1-amd64',     code = 'prg', profile = builder_profiles['openbsd'],          name = 'openbsd-amd64-6.1.prg',    arch = 'amd64',   tlname = 'amd64-openbsd6.1',    upload = True),
-#   BuildWorker(slave = 'pragma-linux-armhf',          code = 'prg', profile = builder_profiles['linux_cxx11'],      name = 'linux-armhf.prg',          arch = 'armhf',   tlname = 'armhf-linux',         upload = True),
-    BuildWorker(slave = 'pragma-linux-debian7-i386',   code = 'prg', profile = builder_profiles['linux'],            name = 'linux-i386-debian7.prg',   arch = 'i386',    tlname = 'i386-linux',          upload = True),
+    BuildWorker(slave = 'pragma-openbsd6.0-i386',      code = 'prg', profile = builder_profiles['openbsd_cxx11'],    name = 'openbsd-i386-6.0.prg',     arch = 'i386',    tlname = 'i386-openbsd6.0',     upload = True),
+    BuildWorker(slave = 'pragma-openbsd6.1-i386',      code = 'prg', profile = builder_profiles['openbsd_cxx11'],    name = 'openbsd-i386-6.1.prg',     arch = 'i386',    tlname = 'i386-openbsd6.1',     upload = True),
+    BuildWorker(slave = 'pragma-openbsd6.0-amd64',     code = 'prg', profile = builder_profiles['openbsd_cxx11'],    name = 'openbsd-amd64-6.0.prg',    arch = 'amd64',   tlname = 'amd64-openbsd6.0',    upload = True),
+    BuildWorker(slave = 'pragma-openbsd6.1-amd64',     code = 'prg', profile = builder_profiles['openbsd_cxx11'],    name = 'openbsd-amd64-6.1.prg',    arch = 'amd64',   tlname = 'amd64-openbsd6.1',    upload = True),
+    BuildWorker(slave = 'pragma-freebsd-i386',         code = 'prg', profile = builder_profiles['freebsd'],          name = 'freebsd-i386.prg',         arch = 'i386',    tlname = 'i386-freebsd',        upload = True),
+    BuildWorker(slave = 'pragma-freebsd-amd64',        code = 'prg', profile = builder_profiles['freebsd'],          name = 'freebsd-amd64.prg',        arch = 'amd64',   tlname = 'amd64-freebsd',       upload = True),
+    BuildWorker(slave = 'pragma-linux-debian9-armhf',  code = 'prg', profile = builder_profiles['linux'],            name = 'linux-armhf-debian9.prg',  arch = 'armhf',   tlname = 'armhf-linux',         upload = True),
+    BuildWorker(slave = 'pragma-linux-debian8-i386',   code = 'prg', profile = builder_profiles['linux'],            name = 'linux-i386-debian8.prg',   arch = 'i386',    tlname = 'i386-linux',          upload = True),
     BuildWorker(slave = 'pragma-linux-debian9-i386',   code = 'prg', profile = builder_profiles['linux'],            name = 'linux-i386-debian9.prg',   arch = 'i386',    tlname = None,                  upload = False),
-    BuildWorker(slave = 'pragma-linux-debian7-x86_64', code = 'prg', profile = builder_profiles['linux'],            name = 'linux-x86_64-debian7.prg', arch = 'x86_64',  tlname = 'x86_64-linux',        upload = True),
+    BuildWorker(slave = 'pragma-linux-debian8-x86_64', code = 'prg', profile = builder_profiles['linux'],            name = 'linux-x86_64-debian8.prg', arch = 'x86_64',  tlname = 'x86_64-linux',        upload = True),
     BuildWorker(slave = 'pragma-linux-debian9-x86_64', code = 'prg', profile = builder_profiles['linux'],            name = 'linux-x86_64-debian9.prg', arch = 'x86_64',  tlname = None,                  upload = False),
 #   BuildWorker(slave = 'pragma-linux-debian9-x86_64', code = 'prg', profile = builder_profiles['linux-mingw32'],    name = 'mingw32-i386.prg',         arch = 'win32',   tlname = 'i686-w64-mingw32',    upload = True),
 #   BuildWorker(slave = 'pragma-linux-debian9-x86_64', code = 'prg', profile = builder_profiles['linux-mingw64'],    name = 'mingw32-x86_64.prg',       arch = 'win64',   tlname = 'x86_64-w64-mingw32',  upload = True),
-    BuildWorker(slave = 'boris-linux-armel',           code = 'bor', profile = builder_profiles['linux_cxx11'],      name = 'linux-armel.bor',          arch = 'armel',   tlname = 'armel-linux',         upload = True),
+    BuildWorker(slave = 'boris-linux-armel',           code = 'bor', profile = builder_profiles['linux'],            name = 'linux-armel.bor',          arch = 'armel',   tlname = 'armel-linux',         upload = True),
+
+#   BuildWorker(slave = 'darwin10-x86_64',             code = 'prg', profile = builder_profiles['darwin10-powerpc'],       name = 'darwin-powerpc.prg', arch = 'powerpc', tlname = 'powerpc-darwin',      upload = True),
+    BuildWorker(slave = 'darwin10-x86_64',             code = 'prg', profile = builder_profiles['darwin10_libc++-i386'],   name = 'darwin-i386.prg',    arch = 'i386',    tlname = 'i386-darwin',         upload = True),
+    BuildWorker(slave = 'darwin10-x86_64',             code = 'prg', profile = builder_profiles['darwin10_libc++-x86_64'], name = 'darwin-x86_64.prg',  arch = 'x86_64',  tlname = 'x86_64-darwinlegacy', upload = True),
 ]
 
